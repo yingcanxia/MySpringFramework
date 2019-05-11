082734aa7aeb6c67b329cd842154ef5ad9242335
yingcanxia <3042004295@qq.com>
2019-05-12 01:05:59 +0800

    513

--- src/main/java/cn/shadow/framework/webmvc/servlet/MyView.java
+++ src/main/java/cn/shadow/framework/webmvc/servlet/MyView.java
@@ -1,12 +1,52 @@
 package cn.shadow.framework.webmvc.servlet;
 
+import java.io.File;
+import java.io.RandomAccessFile;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MyView {
-	public void render(Map<String, ?>model,HttpServletRequest request,HttpServletResponse response)throws Exception {
+	
+	public final String DEFAULT_CONTENT_TYPE="text/html;charset=utf-8";
+	private File viewFile;
+	
+	public MyView(File viewFile) {
+		this.viewFile=viewFile;
 		
+		
+	}
+	
+	public void render(Map<String, ?>model,HttpServletRequest request,HttpServletResponse response)throws Exception {
+		StringBuffer sb=new StringBuffer();
+		RandomAccessFile ra =new RandomAccessFile(viewFile, "r");
+		String line=null;
+		while(null !=(line=ra.readLine())) {
+			line=new String(line.getBytes("ISO-8859-1"),"utf-8");
+			Pattern pattern=Pattern.compile("гд\\{[^]\\}",Pattern.CASE_INSENSITIVE);
+			Matcher matcher =pattern.matcher(line);
+			
+			while(matcher.find()) {
+				String paramName=matcher.group();
+				paramName=paramName.replaceAll("$\\{|\\}","");
+				Object paramValue=model.get(paramName);
+				if(null==paramValue) {
+					continue;
+				}
+				line=matcher.replaceFirst(paramValue.toString());
+				matcher=pattern.matcher(line);
+				
+				
+			}
+			sb.append(line);
+		}
+		
+		
+		response.setCharacterEncoding("utf-8");
+		response.setContentType(DEFAULT_CONTENT_TYPE);
+		response.getWriter().write(sb.toString());
 	}
 }
--- src/main/java/cn/shadow/framework/webmvc/servlet/MyViewResolvers.java
+++ src/main/java/cn/shadow/framework/webmvc/servlet/MyViewResolvers.java
@@ -1,13 +1,34 @@
 package cn.shadow.framework.webmvc.servlet;
 
+import java.io.File;
 import java.util.Locale;
 
 public class MyViewResolvers {
 
+	private final String DEFAULT_TEMPLATE_SUFIX="html";
+	private File templateRootDir;
+	//private String viewName;
+	
 	public MyViewResolvers(String templateRoot) {
 		// TODO Auto-generated constructor stub
+		String templateRootPath=this.getClass().getClassLoader().getResource(templateRoot).getFile();
+		this.templateRootDir=new File(templateRootPath);
 	}
-	MyView resolveViewName(String viewName,Locale locale) {
-		return null;
+	public MyView resolveViewName(String viewName,Locale locale) {
+		if(null==viewName||"".equals(viewName.trim())) {
+			return null;
+		}
+		viewName=viewName.endsWith(DEFAULT_TEMPLATE_SUFIX)? viewName:(viewName + DEFAULT_TEMPLATE_SUFIX);
+		File templateFile=new File((templateRootDir.getPath()+"/"+viewName).replaceAll("/+", "/"));
+		return new MyView(templateFile);
 	}
+	public File getTemplateRootDir() {
+		return templateRootDir;
+	}
+	public void setTemplateRootDir(File templateRootDir) {
+		this.templateRootDir = templateRootDir;
+	}
+
+	
+	
 }
